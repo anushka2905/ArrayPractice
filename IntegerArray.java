422b19dcc090d36a6d09ad58fc0e51d287ebd9b4
anushka2905 <anushkashrivastava29@gmail.com>
2024-09-06 13:28:27 +0530

    neww files

---
+++ IntegerArray.java
@@ -0,0 +1,22 @@
+/*
+    **Integer Array Initialization:**
+    - **Task:** Create an integer array of size 5 and initialize it with the values 10, 20, 30, 40, and 50.
+    - **Sample Input:** None (initialize directly).
+    - **Sample Output:** `[10, 20, 30, 40, 50]` 
+*/
+   import java.util.Arrays;
+    class IntegerArray{
+        public static void main(String[] args){
+            int[] arr = { 10, 20, 30, 40, 50 };
+
+            System.out.print("Elements of given array are: ");
+    
+            // Looping through array by incrementing value of i
+            //'i' is an index of array 'arr'
+            for (int i = 0; i < arr.length; i++) {
+    
+                // Print array element present at index i
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }

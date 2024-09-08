618e35c0a85f88a87b1c773f1c82646686923a29
anushka2905 <anushkashrivastava29@gmail.com>
2024-09-08 10:51:36 +0530

    new file created

---
+++ FloatingPointNumbers.java
@@ -0,0 +1,17 @@
+/*
+ * 13. **Array of Floating-point Numbers:**
+    - **Task:** Create a float array of size 4, initialize it with values, and write a program to print the array.
+    - **Sample Input:** `[1.1f, 2.2f, 3.3f, 4.4f]`
+    - **Sample Output:** `[1.1, 2.2, 3.3, 4.4]`
+ */
+
+import java.util.Arrays;
+
+public class FloatingPointNumbers {
+    public static void main(String[] args){
+        float[] arr = {1.1f, 2.2f, 3.3f, 4.4f};
+        
+        // Print the array
+        System.out.println("Float array: " + Arrays.toString(arr));
+    }   
+}
